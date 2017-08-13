b89be3e50411a5f4b05f19eb6e204bb72c407d8e
Superwoods <st_sister@iCloud.com>
2017-08-13 22:41:25 +0800

    Finsih

--- src/bundle/index.js
+++ src/bundle/index.js
@@ -90,7 +90,7 @@ var _footer = __webpack_require__(5);
 
 var _footer2 = _interopRequireDefault(_footer);
 
-var _swiper = __webpack_require__(8);
+var _swiper = __webpack_require__(6);
 
 var _swiper2 = _interopRequireDefault(_swiper);
 
@@ -115,10 +115,23 @@ $(function () {
         isMobile: isMobile
     });
 
-    var xpageConfigPageSize = 5;
-
     var initPage = function initPage() {
+        // const cms = cmsConfig['台湾青年大陆互联网＋梦想之旅1'].list;
+        // for (let prop in cms) {
+        //     const cid = cms[prop].cid;
+        //     console.log('====================================');
+        //     console.log(prop, cid);
+        //     console.log('====================================');
+        //     // $([data-name=${cms}])
+        //     `<div class="swiper-slide part1-slide">
+        //         <a href="#" target="_blank">
+        //             <img src="${img}">
+        //             <p class="part1-slide-title">1</p>
+        //         </a>
+        //     </div>`
+        // }
 
+        // part1 swiper
         $('#part1 .swiper-container').append('<div class="swiper-pagination swiper-pagination-part1"></div>');
         var mySwiper = new _swiper2.default('#part1 .swiper-container', {
             autoplay: 5000, //可选选项，自动滑动
@@ -126,66 +139,19 @@ $(function () {
             paginationClickable: true
         });
 
+        // part6 swiper
         $('#part6 .swiper-container').append('\n            <div class="swiper-pagination"></div>\n        ');
         $('#part6 .swiper-container').after('\n            <div class="swiper-button-prev" id="part6-swiper-button-prev"></div>\n            <div class="swiper-button-next" id="part6-swiper-button-next"></div>\n        ');
-        var mySwiper = new _swiper2.default('#part6 .swiper-container', {
-            slidesPerView: 3,
+        var mySwiper2 = new _swiper2.default('#part6 .swiper-container', {
+            slidesPerView: isMobile ? 1 : 3,
             slidesPerColumn: 2,
-            spaceBetween: 26,
+            spaceBetween: isMobile ? 10 : 26,
             // autoplay: 5000,//可选选项，自动滑动
             pagination: '#part6 .swiper-pagination',
             paginationClickable: true,
             prevButton: '#part6-swiper-button-prev',
             nextButton: '#part6-swiper-button-next'
         });
-
-        // const lists = cmsConfig['台湾青年大陆互联网＋梦想之旅1'].list;
-        // const xpage = new XHW.Xpage('xpage-content', {
-        //     mode: 'listView', // 插件模式
-        //     nid: lists['大图'].cid, // 栏目id
-        //     pageSize: xpageConfigPageSize, // 每页条数
-        //     moreButton: '.xpage-more-btn', // 更多按钮
-        //     renderItem: function (item, index, list) { // 列表渲染方法
-        //         // ----------- 拼字符串方法 -------
-        //         return (`
-        //             <li class="list-item">
-        //                 <a href="${item.LinkUrl}">
-        //                     ${item.Title}
-        //                 </a>
-        //             </li>
-        //         `);
-        //         //-------------------------------
-        //         // // ----------- 使用模板方法 -------
-        //         // var itemHtml = XHW.Xpage.tmpl('item_tmpl_list', item);
-        //         // return itemHtml;
-        //         // //-------------------------------
-        //     }
-        // });
-
-        // for (let prop in lists) {
-
-        //     const xpage = new XHW.Xpage(`[data-name="${prop}"]`, {
-        //         mode: 'listView', // 插件模式
-        //         nid: lists[prop].cid, // 栏目id
-        //         pageSize: xpageConfigPageSize, // 每页条数
-        //         // moreButton: '.xpage-more-btn', // 更多按钮
-        //         renderItem: function (item, index, list) { // 列表渲染方法
-        //             // ----------- 拼字符串方法 -------
-        //             return (`
-        //                 <li class="list-item">
-        //                     <a href="${item.LinkUrl}">
-        //                         ${item.Title}
-        //                     </a>
-        //                 </li>
-        //             `);
-        //             //-------------------------------
-        //             // // ----------- 使用模板方法 -------
-        //             // var itemHtml = XHW.Xpage.tmpl('item_tmpl_list', item);
-        //             // return itemHtml;
-        //             // //-------------------------------
-        //         }
-        //     });
-        // }
     };
     initPage();
 });
@@ -194,7 +160,7 @@ $(function () {
 /* 1 */
 /***/ (function(module, exports) {
 
-module.exports = {"title":"台湾青年大陆互联网＋梦想之旅","pos":"新华网>台湾频道>专访专题>台湾青年大陆互联网梦想之旅","url":"http://www.xinhuanet.com/tw/zhuanti/twqndlmxzl/index.htm","isFreeMod":true,"台湾青年大陆互联网＋梦想之旅1":{"list":{"大图":{"nid":"11161095","cid":"11160643"},"要闻":{"nid":"11161097","cid":"11160645"},"实习活动":{"nid":"11161098","cid":"11160646"},"参访活动":{"nid":"11161106","cid":"11160654"},"亮点扫描":{"nid":"11161099","cid":"11160647"},"参访企业巡礼":{"nid":"11161100","cid":"11160648"},"精彩瞬间":{"nid":"11161103","cid":"11160651"},"两岸青年交流":{"nid":"11161104","cid":"11160652"}}}}
+module.exports = {"title":"台湾青年大陆互联网＋梦想之旅","pos":"新华网>台湾频道>专访专题>台湾青年大陆互联网梦想之旅","url":"http://www.xinhuanet.com/tw/zhuanti/twqndlmxzl/index.htm","isFreeMod":true,"台湾青年大陆互联网＋梦想之旅1":{"list":{"大图（要闻左侧的）":{"nid":"11161095","cid":"11160643"},"要闻":{"nid":"11161097","cid":"11160645"},"实习活动":{"nid":"11161098","cid":"11160646"},"参访活动":{"nid":"11161106","cid":"11160654"},"亮点扫描":{"nid":"11161099","cid":"11160647"},"参访企业巡礼":{"nid":"11161100","cid":"11160648"},"精彩瞬间":{"nid":"11161103","cid":"11160651"},"两岸青年交流":{"nid":"11161104","cid":"11160652"}}}}
 
 /***/ }),
 /* 2 */
@@ -271,9 +237,7 @@ exports.default = function (_ref) {
 };
 
 /***/ }),
-/* 6 */,
-/* 7 */,
-/* 8 */
+/* 6 */
 /***/ (function(module, exports, __webpack_require__) {
 
 "use strict";
--- src/js/index.js
+++ src/js/index.js
@@ -23,10 +23,23 @@ $(() => {
         isMobile
     });
 
-    const xpageConfigPageSize = 5;
-
     const initPage = () => {
+        // const cms = cmsConfig['台湾青年大陆互联网＋梦想之旅1'].list;
+        // for (let prop in cms) {
+        //     const cid = cms[prop].cid;
+        //     console.log('====================================');
+        //     console.log(prop, cid);
+        //     console.log('====================================');
+        //     // $([data-name=${cms}])
+        //     `<div class="swiper-slide part1-slide">
+        //         <a href="#" target="_blank">
+        //             <img src="${img}">
+        //             <p class="part1-slide-title">1</p>
+        //         </a>
+        //     </div>`
+        // }
 
+        // part1 swiper
         $('#part1 .swiper-container').append(`<div class="swiper-pagination swiper-pagination-part1"></div>`);
         var mySwiper = new Swiper('#part1 .swiper-container', {
             autoplay: 5000,//可选选项，自动滑动
@@ -34,7 +47,7 @@ $(() => {
             paginationClickable: true,
         });
 
-
+        // part6 swiper
         $('#part6 .swiper-container').append(`
             <div class="swiper-pagination"></div>
         `);
@@ -42,65 +55,16 @@ $(() => {
             <div class="swiper-button-prev" id="part6-swiper-button-prev"></div>
             <div class="swiper-button-next" id="part6-swiper-button-next"></div>
         `);
-        var mySwiper = new Swiper('#part6 .swiper-container', {
-            slidesPerView: 3,
+        var mySwiper2 = new Swiper('#part6 .swiper-container', {
+            slidesPerView: isMobile ? 1 : 3,
             slidesPerColumn: 2,
-            spaceBetween: 26,
+            spaceBetween: isMobile ? 10 : 26,
             // autoplay: 5000,//可选选项，自动滑动
             pagination: '#part6 .swiper-pagination',
             paginationClickable: true,
             prevButton: '#part6-swiper-button-prev',
             nextButton: '#part6-swiper-button-next',
         });
-
-
-        // const lists = cmsConfig['台湾青年大陆互联网＋梦想之旅1'].list;
-        // const xpage = new XHW.Xpage('xpage-content', {
-        //     mode: 'listView', // 插件模式
-        //     nid: lists['大图'].cid, // 栏目id
-        //     pageSize: xpageConfigPageSize, // 每页条数
-        //     moreButton: '.xpage-more-btn', // 更多按钮
-        //     renderItem: function (item, index, list) { // 列表渲染方法
-        //         // ----------- 拼字符串方法 -------
-        //         return (`
-        //             <li class="list-item">
-        //                 <a href="${item.LinkUrl}">
-        //                     ${item.Title}
-        //                 </a>
-        //             </li>
-        //         `);
-        //         //-------------------------------
-        //         // // ----------- 使用模板方法 -------
-        //         // var itemHtml = XHW.Xpage.tmpl('item_tmpl_list', item);
-        //         // return itemHtml;
-        //         // //-------------------------------
-        //     }
-        // });
-
-        // for (let prop in lists) {
-
-        //     const xpage = new XHW.Xpage(`[data-name="${prop}"]`, {
-        //         mode: 'listView', // 插件模式
-        //         nid: lists[prop].cid, // 栏目id
-        //         pageSize: xpageConfigPageSize, // 每页条数
-        //         // moreButton: '.xpage-more-btn', // 更多按钮
-        //         renderItem: function (item, index, list) { // 列表渲染方法
-        //             // ----------- 拼字符串方法 -------
-        //             return (`
-        //                 <li class="list-item">
-        //                     <a href="${item.LinkUrl}">
-        //                         ${item.Title}
-        //                     </a>
-        //                 </li>
-        //             `);
-        //             //-------------------------------
-        //             // // ----------- 使用模板方法 -------
-        //             // var itemHtml = XHW.Xpage.tmpl('item_tmpl_list', item);
-        //             // return itemHtml;
-        //             // //-------------------------------
-        //         }
-        //     });
-        // }
     };
     initPage();
 });
